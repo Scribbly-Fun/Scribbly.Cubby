5532b26036baa2760abd9e9ff5503a3b96461372
Eric Williams <ewilliams0305@gmail.com>
2025-12-22 09:08:13 -0500

    added benchmark to update the same entry in the cache

---
+++ source/Scribbly.Cubby.Aspire/CubbyResource.cs
@@ -0,0 +1,14 @@
+﻿using Aspire.Hosting.ApplicationModel;
+
+namespace Scribbly.Cubby;
+
+/// <summary>
+/// 
+/// </summary>
+public class CubbyResource : ProjectResource
+{
+    /// <inheritdoc />
+    internal CubbyResource(string name) : base(name)
+    {
+    }
+}
---
+++ source/Scribbly.Cubby.Aspire/CubbyResourceBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Aspire.Hosting;
+using Aspire.Hosting.ApplicationModel;
+
+namespace Scribbly.Cubby;
+
+/// <summary>
+/// 
+/// </summary>
+public static class CubbyResourceBuilderExtensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="builder"></param>
+    extension(IDistributedApplicationBuilder builder)
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public IResourceBuilder<CubbyResource> AddCubby([ResourceName] string name = "scrb-cubby")
+        {
+            ArgumentNullException.ThrowIfNull(builder);
+            ArgumentException.ThrowIfNullOrEmpty(name);
+        
+            var resource = new CubbyResource(name);
+        
+            var resourceBuilder = builder
+                .AddResource(resource)
+                .WithIconName("TopSpeed");
+        
+            return resourceBuilder;
+        }
+    }
+}
