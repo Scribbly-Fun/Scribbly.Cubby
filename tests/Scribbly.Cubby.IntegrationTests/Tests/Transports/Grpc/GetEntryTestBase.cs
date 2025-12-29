503849a0db51f33a9e473866c267813026e04980
Eric Williams <ewilliams0305@gmail.com>
2025-12-29 07:52:27 -0500

    added grpc http handler callback used to bind grpc channel to app host

---
+++ tests/Scribbly.Cubby.IntegrationTests/Tests/Transports/Grpc/GetEntryTestBase.cs
@@ -0,0 +1,8 @@
+﻿using Scribbly.Cubby.IntegrationTests.Setup;
+
+namespace Scribbly.Cubby.IntegrationTests.Tests.Transports.Grpc;
+
+
+[Collection(Collections.GrpcCollection)]
+public class GetEntryTestBase(GrpcApplicationFactory application) 
+    : Get_Entry_Client_TestBase<Program>(application);
