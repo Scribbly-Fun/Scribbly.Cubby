638a802145ad058a9e48e95851d655ebf2456649
Eric Williams <ewilliams0305@gmail.com>
2025-12-30 14:24:00 -0500

    updated put tests

--- tests/Scribbly.Cubby.IntegrationTests/Tests/Transports/Put_Entry_Client_TestBase.cs
+++ tests/Scribbly.Cubby.IntegrationTests/Tests/Transports/Put_Entry_Client_TestBase.cs
@@ -9,11 +9,11 @@ namespace Scribbly.Cubby.IntegrationTests.Tests.Transports;
 public abstract class Put_Entry_Client_TestBase<T>(WebApplicationFactory<T> application) where T : class
 {
     [Theory]
-    [InlineData("new key 1", 2000)]
-    [InlineData("new key 2", 199_999)]
-    [InlineData("new key 3", 2)]
-    [InlineData("new key 4", 65_535)]
-    [InlineData("new key 5", 199_999)]
+    [InlineData(nameof(Given_NewKey_Put_Should_Return_Created) + "key 1" + nameof(T), 2000)]
+    [InlineData(nameof(Given_NewKey_Put_Should_Return_Created) + "key 2" + nameof(T), 199_999)]
+    [InlineData(nameof(Given_NewKey_Put_Should_Return_Created) + "key 3" + nameof(T), 2)]
+    [InlineData(nameof(Given_NewKey_Put_Should_Return_Created) + "key 4" + nameof(T), 65_535)]
+    [InlineData(nameof(Given_NewKey_Put_Should_Return_Created) + "key 5" + nameof(T), 199_999)]
     public async Task Given_NewKey_Put_Should_Return_Created(string key, int length)
     {
         byte[] array = new byte[length];
@@ -28,11 +28,11 @@ public abstract class Put_Entry_Client_TestBase<T>(WebApplicationFactory<T> appl
     }
 
     [Theory]
-    [InlineData("new key 1", 2000)]
-    [InlineData("new key 2", 199_999)]
-    [InlineData("new key 3", 2)]
-    [InlineData("new key 4", 65_535)]
-    [InlineData("new key 5", 199_999)]
+    [InlineData(nameof(Given_NewKey_Put_Should_Create_Entry) + "key 1" + nameof(T), 2000)]
+    [InlineData(nameof(Given_NewKey_Put_Should_Create_Entry) + "key 2" + nameof(T), 199_999)]
+    [InlineData(nameof(Given_NewKey_Put_Should_Create_Entry) + "key 3" + nameof(T), 2)]
+    [InlineData(nameof(Given_NewKey_Put_Should_Create_Entry) + "key 4" + nameof(T), 65_535)]
+    [InlineData(nameof(Given_NewKey_Put_Should_Create_Entry) + "key 5" + nameof(T), 199_999)]
     public async Task Given_NewKey_Put_Should_Create_Entry(string key, int length)
     {
         byte[] array = new byte[length];
@@ -48,11 +48,11 @@ public abstract class Put_Entry_Client_TestBase<T>(WebApplicationFactory<T> appl
     }
 
     [Theory]
-    [InlineData("key", 2000)]
-    [InlineData("key", 199_999)]
-    [InlineData("key", 2)]
-    [InlineData("key", 65_535)]
-    [InlineData("key", 7_994)]
+    [InlineData(nameof(Given_ExistingKey_Put_Should_Return_Updated) + "key" + nameof(T), 2000)]
+    [InlineData(nameof(Given_ExistingKey_Put_Should_Return_Updated) + "key" + nameof(T), 199_999)]
+    [InlineData(nameof(Given_ExistingKey_Put_Should_Return_Updated) + "key" + nameof(T), 2)]
+    [InlineData(nameof(Given_ExistingKey_Put_Should_Return_Updated) + "key" + nameof(T), 65_535)]
+    [InlineData(nameof(Given_ExistingKey_Put_Should_Return_Updated) + "key" + nameof(T), 7_994)]
     public async Task Given_ExistingKey_Put_Should_Return_Updated(string key, int length)
     {
         byte[] array = new byte[length];
@@ -61,17 +61,19 @@ public abstract class Put_Entry_Client_TestBase<T>(WebApplicationFactory<T> appl
         using var scope = application.Services.CreateScope();
         var client = scope.ServiceProvider.GetRequiredService<ICubbyClient>();
         
+        await client.Put(key, array, new CacheEntryOptions(), CancellationToken.None);
+        
         var result = await client.Put(key, array, new CacheEntryOptions(), CancellationToken.None);
 
         result.Should().Be(PutResult.Updated);
     }
 
     [Theory]
-    [InlineData("key", 2000)]
-    [InlineData("key", 199_999)]
-    [InlineData("key", 2)]
-    [InlineData("key", 65_535)]
-    [InlineData("key", 7_994)]
+    [InlineData(nameof(Given_ExistingKey_Put_Should_Return_Updated) + "key" + nameof(T), 2000)]
+    [InlineData(nameof(Given_ExistingKey_Put_Should_Return_Updated) + "key" + nameof(T), 199_999)]
+    [InlineData(nameof(Given_ExistingKey_Put_Should_Return_Updated) + "key" + nameof(T), 2)]
+    [InlineData(nameof(Given_ExistingKey_Put_Should_Return_Updated) + "key" + nameof(T), 65_535)]
+    [InlineData(nameof(Given_ExistingKey_Put_Should_Return_Updated) + "key" + nameof(T), 7_994)]
     public async Task Given_NewKey_Put_Should_Update_Entry(string key, int length)
     {
         byte[] array = new byte[length];
