392a6bc371c27de5ccdd3b7b84c4ba6ec6a98212
Eric Williams <ewilliams0305@gmail.com>
2026-01-04 21:20:52 -0500

    added generator to create docker tag number

---
+++ source/Scribbly.Cubby.Aspire.Analyzer/ContainerTagGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Scribbly.Cubby;
+
+/// <inheritdoc />
+[Generator(LanguageNames.CSharp)]
+public sealed class ContainerTagGenerator : IIncrementalGenerator
+{
+    /// <inheritdoc />
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        IncrementalValueProvider<string?> property = context
+            .AnalyzerConfigOptionsProvider
+            .Select((options, _) =>
+            {
+                options.GlobalOptions.TryGetValue(
+                    "build_property.CubbyContainerImageTag",
+                    out var value);
+
+                return value;
+            });
+
+        context.RegisterSourceOutput(property, (spc, value) =>
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return;
+
+            var source = $$"""
+                           namespace Scribbly.Cubby
+                           {
+                               internal static class BuildConstants
+                               {
+                                   public const string ContainerImageTag = "{{value}}";
+                               }
+                           }
+                           """;
+
+            spc.AddSource(
+                "BuildConstants.g.cs",
+                SourceText.From(source, Encoding.UTF8));
+        });
+    }
+}
