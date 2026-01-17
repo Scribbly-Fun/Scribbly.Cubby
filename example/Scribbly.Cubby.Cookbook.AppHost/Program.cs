6a29a703d907ebc9293abd2c9f2f7ae4a3a177bb
Eric Williams <53901011+ewilliams0305@users.noreply.github.com>
2026-01-16 21:19:34 -0500

    Update Program.cs

--- example/Scribbly.Cubby.Cookbook.AppHost/Program.cs
+++ example/Scribbly.Cubby.Cookbook.AppHost/Program.cs
@@ -7,24 +7,24 @@ var builder = DistributedApplication.CreateBuilder(args);
 var cubbyAot = builder.AddProject<Projects.Scribbly_Cubby_Host>("scrb-cubby")
     .WithEnvironment("SCRB_CUBBY_HTTPS", "True");
 
-var cubbyDockerFile = builder
-    .AddDockerfile("scrb-cubby-dockerfile", "../../", "Dockerfile");
+//var cubbyDockerFile = builder
+//    .AddDockerfile("scrb-cubby-dockerfile", "../../", "Dockerfile");
 
-var cubbyAdminPortal = builder
-    .AddDockerfile("scrb-cubby-portal", "../../portal", "Dockerfile")
-    .WithEndpoint(3002, 3000, "http")
-    .WithReference(cubbyAot);
+//var cubbyAdminPortal = builder
+//    .AddDockerfile("scrb-cubby-portal", "../../portal", "Dockerfile")
+//    .WithEndpoint(3002, 3000, "http")
+//    .WithReference(cubbyAot);
 
 var cubbyContainer = builder
     .AddCubbyContainer("scrb-cubby-container");
 
-cubbyContainer.WithCubbyPortal("portal");
+//cubbyContainer.WithCubbyPortal("portal");
 
 var cookbook = builder.AddProject<Projects.Scribbly_Cubby_Cookbook_ApiService>("scrb-cookbook")
     .WithReference(cubbyAot)
-    .WaitFor(cubbyAot)
-    .WithReference(cubbyContainer)
-    .WaitFor(cubbyContainer);
+    .WaitFor(cubbyAot);
+   // .WithReference(cubbyContainer)
+   // .WaitFor(cubbyContainer);
 
 if (builder.ExecutionContext.IsRunMode)
 {
